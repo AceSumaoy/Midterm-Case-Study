78b67bc25fcc3e9077e960bf345bcb74977c7cef
AceSumaoy <sumaoyace78@gmail.com>
2024-05-08 10:43:26 +0800

    Add Routing and Controllers

---
+++ backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index() {
+        $products = Products::all();
+        return response()->json($products);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource("/products", ProductController::class);
+Route::post('/checkout', [CartController::class, 'checkout']);
