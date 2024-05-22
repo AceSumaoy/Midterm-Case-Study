c2dedc8cc484c4137ccdaf621b71f7b59aea34d6
Mhaerrick S. Riego de Dios <riegodediosmhaerrick14@gmail.com>
2024-05-22 14:50:45 +0800

    Storing and Creating User Acc

--- backend/app/Http/Controllers/ManageUserController.php
+++ backend/app/Http/Controllers/ManageUserController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ManageUserController extends Controller
 {
-    //
+    // Display a listing of users.
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        //Add or Create User
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
 }
