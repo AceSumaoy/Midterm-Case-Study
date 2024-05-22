b08a9d5b3b4a189db42b364e99c4af226315e6d1
LA Santos <123438824+BlankPhantom@users.noreply.github.com>
2024-05-22 14:43:04 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\AddProductController;
+use App\Http\Controllers\RemoveProdController;
+use App\Http\Controllers\UpdateProdController;
+use App\Http\Controllers\ManageUserController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +16,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource("/products", ProductController::class);
 Route::post('/checkout', [CartController::class, 'checkout']);
+
